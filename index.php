bcac6f208506cd97e4d2d99e34f8f3e70bcc6fb7
Noam B Zilberstein <noamz@minus.seas.upenn.edu>
2013-04-02 01:20:41 -0400

    noam update1

--- index.php
+++ index.php
@@ -1,23 +1,66 @@
 <!DOCTYPE html>
 <html>
-<body>
+<head>
+  <style>
+    body {
+    background-color: #333;
+    font-family: sans-serif;
+    }
 
-<h1>hello world</h1>
+    #main {
+    margin: 0px auto;
+    padding: 25px;
+    border-radius: 25px;
+    width: 75%;
+    background-color: #eee;
+    box-shadow: 0px 0px 25px #000;
+    }
+
+    h1 {
+    text-shadow: 1px 1px 1px #666;
+    }
+
+    .graph {
+    position: relative;
+    padding: 5px;
+    margin: 5px;
+    border-radius: 5px;
+    box-shadow: 2px 2px #000;
+    color: #fff;
+    }
+  </style>
+</head>
+<body>
+<div id="main">
+<h1>Welcome to My Beautiful Website!</h1>
 
 <?php
 echo "page view: ";
 $IP =  $_SERVER["REMOTE_ADDR"];
 echo $IP;
-$command = "python /home1/n/noamz/html/cgi-bin/update_data $IP";
+$temp = $_SERVER['HTTP_USER_AGENT'];
+if (strpos($temp,'iPhone') !== false || strpos($temp, 'iPad') !== false || strpos($temp,'iPod') !== false)
+   $OS = "iOS";
+else if (strpos($temp,'Windows') !== false)
+   $OS = "WINDOWS";
+else if (strpos($temp,'Macintosh') !== false)
+   $OS = "MAC";
+else
+   $OS = "OTHER";
+
+$command = "python /home1/n/noamz/html/cgi-bin/update_data $IP $OS";
 echo '<br>';
-echo $command;
 $a = '100px';
-echo "<div style=\"background-color:#00f;position:fixed;top:125px;width: $a\">Mac</div>";
-echo "<div style=\"background-color:#0f0;position:fixed;top:150px;width: 150px\">Windows</div>";
-echo $_SERVER['HTTP_USER_AGENT'];
+echo "<div class=\"graph\" style=\"background-color:#156086;width: $a;\">Mac</div>";
+echo "<div class=\"graph\" style=\"background-color:#7d1586;width: 150px;\">Windows</div>";
+echo "<div class=\"graph\" style=\"background-color:#861522;width: 75px;\">iOS</div>";
+echo "<div class=\"graph\" style=\"background-color:#f87705;width: 60px;\">Android</div>";
+echo "<div class=\"graph\" style=\"background-color:#37a721;width: 40px;\">Other</div>";
+
 $temp = exec($command, $output);
 
 ?>
+</div>
 
 </body>
 </html>
